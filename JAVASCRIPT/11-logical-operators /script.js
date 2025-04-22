7517fbb380699f8dc5ca02cfb7ddd9f747088b0f
GAMERXZ095 <gamera2.057@gmail.com>
2025-04-22 18:08:49 +0530

    update

--- JAVASCRIPT/11-logical-operators /script.js
+++ JAVASCRIPT/11-logical-operators /script.js
@@ -1,3 +1,7 @@
-const userAge = 18
+const userAge = 24;
 
-const isCollageStudent = userAge >= 18 && userAge <= 24
+const isSchoolStudent = userAge >= 5 && userAge <= 18;
+const isCollageStudent = userAge >= 18 && userAge <= 24;
+
+const isStudent = isSchoolStudent || isCollageStudent
+console.log(isStudent);
