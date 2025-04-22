564f1624605efba0a5cb4bbcf4b116213013c7f6
GAMERXZ095 <gamera2.057@gmail.com>
2025-04-22 19:03:56 +0530

    update

---
+++ JAVASCRIPT/12-DECISION-MAKING-IN-JAVASCRIPT/script.js
@@ -0,0 +1,24 @@
+let userName = prompt('Enter Your Name.')
+let userAge = +prompt('Enter Your Age.')
+let gender = prompt('Enter Your Age')
+if (userName === null) {
+  userName = 'procoder'
+}
+
+if (userAge === 0) {
+  userAge = 20
+}
+
+console.log(`Name: ${userName}`)
+console.log(`Age: ${userAge}`)
+
+if (userAge >= 18 && userAge <= 24) {
+  console.log(`${userName} is collage student.`)
+}
+if (gender == 'male') {
+  console.log(And )
+}
+
+if (userAge >= 25 && userAge <= 45) {
+  console.log(`${userName} Is a working professional.`)
+}
