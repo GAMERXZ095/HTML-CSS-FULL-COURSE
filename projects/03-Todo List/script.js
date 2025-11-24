3cd6ab3c4a2b37a74e7d17a36e1dc9e0d518480a
GAMERXZ095 <gamera2057@gmail.com>
2025-11-24 22:23:45 +0530

    update

---
+++ projects/03-Todo List/script.js
@@ -0,0 +1,102 @@
+const inputBox = document.getElementById('inputBox')
+const addBtn = document.getElementById('addBtn')
+const todoList = document.getElementById('todoList')
+let editTodo = null
+
+//add Todo Function 
+const addTodo = () => {
+  const inputText = inputBox.value.trim()
+  if (inputText.length <= 0) {
+    alert('Write Something In Your Todo')
+    return false;
+  }
+  
+  if (addBtn.value === 'Edit') {
+    editTodo.target.previousElementSibling.innerHTML = inputText
+    addBtn.value = 'Add'
+    inputBox.value = ''
+  } else {
+    
+    // creating li and p
+    const li = document.createElement('li')
+    const p = document.createElement('p')
+    p.innerHTML = inputText
+    li.appendChild(p)
+    
+    //Edit Button 
+    const editBtn = document.createElement('button')
+    editBtn.innerText = ('Edit')
+    editBtn.classList.add('btn', 'editBtn')
+    li.appendChild(editBtn)
+    
+    //Delete Button 
+    const delBtn = document.createElement('button')
+    delBtn.innerText = ('Remove')
+    delBtn.classList.add('btn', 'delBtn')
+    li.appendChild(delBtn)
+    
+    todoList.appendChild(li)
+    inputBox.value = ''
+    saveLocalTodo(inputText)
+  }
+}
+
+// Update todo function
+const updateTodo = (e) => {
+  //console.log(e.target)
+  if (e.target.innerHTML === 'Remove') {
+    console.log(e.target.parentElement)
+    todoList.removeChild(e.target.parentElement)
+  }
+  
+  if (e.target.innerHTML === 'Edit') {
+    inputBox.value = e.target.previousElementSibling.innerHTML
+    inputBox.focus()
+    addBtn.value = 'Edit'
+    editTodo = e
+  }
+}
+
+const saveLocalTodo = (todo) => {
+  let todos
+  if(localStorage.getItem('todos') === null){
+    todos = []
+  }else{
+    todos = JSON.parse(localStorage.getItem('todos'))
+  }
+  todos.push(todo)
+  localStorage.setItem('todos',JSON.stringify(todos))
+}
+
+const getLocalTodo = () => {
+  if (localStorage.getItem('todos') === null) {
+  todos = []
+} else {
+  todos = JSON.parse(localStorage.getItem('todos'))
+  todos.forEach(todo => {
+        // creating li and p
+    const li = document.createElement('li')
+    const p = document.createElement('p')
+    p.innerHTML = todo
+    li.appendChild(p)
+    
+    //Edit Button 
+    const editBtn = document.createElement('button')
+    editBtn.innerText = ('Edit')
+    editBtn.classList.add('btn', 'editBtn')
+    li.appendChild(editBtn)
+    
+    //Delete Button 
+    const delBtn = document.createElement('button')
+    delBtn.innerText = ('Remove')
+    delBtn.classList.add('btn', 'delBtn')
+    li.appendChild(delBtn)
+    
+    todoList.appendChild(li)
+  })
+}
+}
+
+document.addEventListener('DOMContentLoaded', getLocalTodo);
+addBtn.addEventListener('click', addTodo)
+todoList.addEventListener('click', updateTodo)
